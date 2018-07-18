1e394e163ee5d76b87dedd3bd2b822fd2476f73a
tyhye <wzy0304@outlook.com>
2018-07-18 14:57:06 +0800

    classlabel

---
+++ classlabel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+# -*- code:utf-8 -*- 
+'''
+ @Author: tyhye.wang 
+ @Date: 2018-07-05 13:43:02 
+ @Last Modified by:   tyhye.wang 
+ @Last Modified time: 2018-07-18 14:39:44 
+'''
+
+import tkinter as tk
+from tktools.tools.classlabel import ClassLabel
+
+def main():
+    
+    window = tk.Tk()
+    window.geometry("960x540")
+    # l = tk.Label(window, text="Hello World!")
+    # l.pack()
+    # vdl = VideoLabel(window)
+    cl = ClassLabel(window)
+    # dp =  Duplicate(window)
+    window.mainloop()
+    
+if __name__ == "__main__":
+    main()
